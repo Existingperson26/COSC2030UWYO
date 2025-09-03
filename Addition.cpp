9c3e256735a2b82a4744f1b3dc7315e99a2a0254
Existingperson26 <pmarti27@uwyo.edu>
2025-09-02 18:10:34 -0600

    Initial Commit

--- Addition.cpp
+++ Addition.cpp
@@ -3,15 +3,23 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+int AddUp(char*A[], int n);
+
 int main(int argc, char*argv[]){
 	if (argc - 1 <= 10){
-		int sum = 0;
-		for (int i = 1; i < argc; i++){
-			sum += std::atoi(argv[i]);
-	}
-		cout << "The sum of the " << argc - 1 << " numbers submitted is: " << sum << "!" <<  endl;
+		int finalSum = AddUp(argv, argc);
+		cout << "The sum of the " << argc - 1 << " numbers submitted is: " << finalSum << "!" <<  endl;
 	} else {
 		cout << "Error! Too many inputs! Please only insert 10 numbers!" << endl;
 	}
 	return 0;
 }
+
+int AddUp(char*A[], int n){
+	int sum = 0;
+	for(int i = 1; i < n; i++){
+		sum += std::atoi(A[i]);
+	}
+	return sum;
+}
+
